10c2513cb8e13f066a717480ba0a27256375666c
Farmaan16 <farmanmuzammil17gmail.com>
2024-04-05 12:57:55 +0530

    "Added spotlight and Moving border"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[o.96] antialiased bg-grid-white/[0.2]">
-      <h1 className="text-3xl text-center font-bold mt-6">Hello</h1>
+      <HeroSection/>
     </main>
   );
 }
