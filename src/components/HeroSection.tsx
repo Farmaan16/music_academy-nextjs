289c095e9464f1b581edb0b138fd13218909441b
Farmaan16 <farmanmuzammil17gmail.com>
2024-04-05 14:46:22 +0530

    "Added sticky-scroll"

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,13 +4,13 @@ import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+    <div className="h-screen md:h-screen w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
       <div className="p-4 relative z-10 w-full text-center">
-        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-stone-50 to-stone-400 ">
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-stone-50 to-stone-400 lg:text-6xl sm:text-base">
           Master the art of music
         </h1>
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-100 max-w-lg mx-auto">
